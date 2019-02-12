15e5bb6d4a2b8decc571884b623c6299d4226997
Admin <admin@MAC12.local>
2019-02-12 11:00:24 +0700

    first commit

---
+++ Multiview/SecondViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SecondViewController.swift
+//  Multiview
+//
+//  Created by Admin on 12/2/2562 BE.
+//  Copyright © 2562 AryMiku. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToSegue(segue : UIStoryboardSegue){
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Multiview/ViewController.swift
+++ Multiview/ViewController.swift
@@ -12,7 +12,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationItem.title = "Menu"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(showItems))
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @objc func showItems(){
+        print("fffff")
+    }
+    
+    @IBAction func unwindToSegue(segue : UIStoryboardSegue){
+        
     }
 
 
